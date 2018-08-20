dbb6aef619e13c183f8bbe4c53679ce25f86ad99
summerkidd8 <fukunxia@hotmail.com>
2018-08-20 14:22:59 +1000

    creat the first learning program

---
+++ learning1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 20 14:12:40 2018
+
+@author: fxia
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=[0,1]
+y=[0,1]
+plt.figure()
+plt.plot(x,y)
+plt.savefig('learning1.jpg')
